b75e4cdea0c89bbf3afd8ac1f422b7f8819bc578
Siraj Lakhani <sirajlak@gmail.com>
2026-02-19 22:50:16 +0000

    Searh tools

--- main.py
+++ main.py
@@ -2,28 +2,32 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-from langchain_google_genai import GoogleGenerativeAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain.tools import tool
 from langchain.agents import create_agent
 from langchain_core.messages import HumanMessage
+#from tavily import TavilyClient
+from langchain_tavily import TavilySearch
 
+# tavily_client = TavilyClient(api_key=os.environ.get("TAVILY_API_KEY"))
 
+# @tool
+# def search(query: str) -> str:
+#     """Search the web for information"""
+#     print(f"Searching the web for information: {query}")
+#     return tavily_client.search(query)
 
-@tool
-def search(query: str) -> str:
-    """Search the web for information"""
-    return "I found this information: " + query
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.environ.get("GOOGLE_API_KEY"))
 
-llm = GoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.environ.get("GOOGLE_API_KEY"))
-
-tools = [search]
+#tools = [search]
+tools = [TavilySearch()]
 agent = create_agent(model=llm,tools=tools)
 
 
 def main():
     print("Hello from lang-hello!")
-    response = agent.invoke({"messages": [HumanMessage(content="What is the capital of India?")]})
-    print(response)
+    response = agent.invoke({"messages": [HumanMessage(content="Serach for 3 Job openings in the field of AI and Machine Learning from Linkedin?")]})    
+    print(f"Response: {response}")
  
 if __name__ == "__main__":
     main()

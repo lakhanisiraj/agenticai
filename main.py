5085f706d7606663b00a709af01a633d5858ac5c
Siraj Lakhani <sirajlak@gmail.com>
2026-02-19 22:17:37 +0000

    Refactor main.py to implement a web search tool and integrate it with a generative AI agent. Removed commented-out code and added functionality to invoke the agent with user queries.

--- main.py
+++ main.py
@@ -1,45 +1,29 @@
 from dotenv import load_dotenv
-from langchain_core.prompts import ChatPromptTemplate
-from langchain_google_genai import GoogleGenerativeAI
-from langchain_core.prompts import PromptTemplate
-from langchain_core.messages import HumanMessage, SystemMessage
 import os
-
 load_dotenv()
 
-# llm = GoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.environ.get("GOOGLE_API_KEY"))
+from langchain_google_genai import GoogleGenerativeAI
+from langchain.tools import tool
+from langchain.agents import create_agent
+from langchain_core.messages import HumanMessage
 
-# prompt = ChatPromptTemplate.from_messages([
-#     SystemMessage(content="You are a helpful assistant that can answer questions and help with tasks."),
-#     HumanMessage(content="What is the capital of India?")
-# ])
 
-# chain = prompt | llm
 
-# response = chain.invoke({"input": "What is the capital of France?"})
+@tool
+def search(query: str) -> str:
+    """Search the web for information"""
+    return "I found this information: " + query
 
-# print(response)
+llm = GoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.environ.get("GOOGLE_API_KEY"))
+
+tools = [search]
+agent = create_agent(model=llm,tools=tools)
+
 
 def main():
     print("Hello from lang-hello!")
-
-    llm = GoogleGenerativeAI(model="gemini-2.5-flash", google_api_key=os.environ.get("GOOGLE_API_KEY"))
-    information = """Elon Reeve Musk (/ˈiːlɒn/ EE-lon; born June 28, 1971) is a businessman and entrepreneur known for his leadership of Tesla, SpaceX, Twitter, and xAI. Musk has been the wealthiest person in the world since 2025; as of February 2026, Forbes estimates his net worth to be around US$852 billion.
-
-Born into a wealthy family in Pretoria, South Africa, Musk emigrated in 1989 to Canada; he has Canadian citizenship since his mother was born there. He received bachelor's degrees in 1997 from the University of Pennsylvania before moving to California to pursue business ventures. In 1995, Musk co-founded the software company Zip2. Following its sale in 1999, he co-founded X.com, an online payment company that later merged to form PayPal, which was acquired by eBay in 2002. Musk also became an American citizen in 2002.
-
-In 2002, Musk founded the space technology company SpaceX, becoming its CEO and chief engineer; the company has since led innovations in reusable rockets and commercial spaceflight. Musk joined the automaker Tesla as an early investor in 2004 and became its CEO and product architect in 2008; it has since become a leader in electric vehicles. In 2015, he co-founded OpenAI to advance artificial intelligence (AI) research, but later left; growing discontent with the organization's direction and their leadership in the AI boom in the 2020s led him to establish xAI, which became a subsidiary of SpaceX in 2026. In 2022, he acquired the social network Twitter, implementing significant changes, and rebranding it as X in 2023. His other businesses include the neurotechnology company Neuralink, which he co-founded in 2016, and the tunneling company the Boring Company, which he founded in 2017. In November 2025, a Tesla pay package worth $1 trillion for Musk was approved, which he is to receive over 10 years if he meets specific goals."""
-    
-    summary_template = """
-    Given the following information about a person, extract a short summary:
-    {information}
-    """
-    summary_prompt_template = PromptTemplate(template=summary_template, input_variables=["information"])
-    llm_chain = summary_prompt_template | llm
-    summary = llm_chain.invoke({"information": information})
-
-    print(summary)
-
+    response = agent.invoke({"messages": [HumanMessage(content="What is the capital of India?")]})
+    print(response)
  
 if __name__ == "__main__":
     main()
